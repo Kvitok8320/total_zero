af45fd0d5a136de2bd72df19254c98d5b0e334b8
Kvitok8320 <153814702+Kvitok8320@users.noreply.github.com>
2024-07-28 22:51:24 +0500

    made template

--- total/order/forms.py
+++ total/order/forms.py
@@ -1,12 +1,17 @@
 from django import forms
-from .models import Order
+from .models import Order, OrderItem
 from register.models import User
 from catalog.models import Product
 
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['user', 'products']
-        widgets = {
-            'products': forms.CheckboxSelectMultiple(),
-        }
+        fields = ['user']
+
+class OrderItemForm(forms.ModelForm):
+    product = forms.ModelChoiceField(queryset=Product.objects.all())
+    quantity = forms.IntegerField(min_value=1, initial=1)
+
+    class Meta:
+        model = OrderItem
+        fields = ['product', 'quantity']
--- total/order/views.py
+++ total/order/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Order, OrderItem
+from .forms import OrderForm, OrderItemForm
 
-# Create your views here.
+def create_order(request):
+    if request.method == 'POST':
+        order_form = OrderForm(request.POST)
+        order_item_formset = OrderItemForm(request.POST)
+
+        if order_form.is_valid() and order_item_formset.is_valid():
+            order = order_form.save()
+            order_item = order_item_formset.save(commit=False)
+            order_item.order = order
+            order_item.save()
+            return redirect('order_success')
+        else:
+            order_form = OrderForm()
+            order_item_formset = OrderItemForm()
+
+        return render(request, 'create_order.html',
+                      {'order_form': order_form, 'order_item_formset': order_item_formset})

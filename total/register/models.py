12e5d72a62ab0222bd229fc97c1d570f851acefd
Kvitok8320 <153814702+Kvitok8320@users.noreply.github.com>
2024-07-29 23:31:41 +0500

    begin add registration

--- total/register/models.py
+++ total/register/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    tgnic = models.CharField(max_length=50)
+    tgnic = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
--- total/register/views.py
+++ total/register/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('registration_success')
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'register/register.html', {'form': form})
+
+
+def registration_success(request):
+    return render(request, 'register/registration_success.html')

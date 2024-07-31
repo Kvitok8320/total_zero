676282a1b9e5b21213a202d133c5d5742e04bae6
Kvitok8320 <153814702+Kvitok8320@users.noreply.github.com>
2024-07-31 23:15:07 +0500

    edit model product

--- total/catalog/models.py
+++ total/catalog/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='catalog/img/', blank=True, null=True)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name

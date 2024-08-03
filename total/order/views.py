18c75bad2761329c5bc9338d38ba48b7e17522f7
Kvitok8320 <153814702+Kvitok8320@users.noreply.github.com>
2024-08-03 19:26:15 +0500

    created button add in cards in catalog

--- total/order/views.py
+++ total/order/views.py
@@ -1,7 +1,38 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from .models import Order, OrderItem
-from .forms import OrderForm, OrderItemForm
+from .forms import OrderForm, OrderItemForm, Product
+from django.views.decorators.http import require_POST
+from .cart import Cart
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.add(product=product, quantity=int(request.POST.get('quantity', 1)))
+    return redirect('cart_detail')
+
+@require_POST
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart_detail')
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart': cart})
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        order = Order.objects.create(user=request.user)
+        for item in cart:
+            OrderItem.objects.create(order=order, product=item['product'], quantity=item['quantity'])
+        cart.clear()
+        return render(request, 'cart/order_created.html', {'order': order})
+    return redirect('cart_detail')
+
 @login_required
 def create_order(request):
     if request.method == 'POST':
--- total/total/settings.py
+++ total/total/settings.py
@@ -134,3 +134,4 @@ LOGIN_URL = '/register/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+CART_SESSION_ID = 'cart'
